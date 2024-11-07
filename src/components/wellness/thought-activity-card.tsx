2021954b55bd1ede8715abf17be5457d3500b18e
joelriveradev <joelriveradev@gmail.com>
2024-11-07 11:21:42 -0600

    chore: integrate post length component

--- src/components/wellness/thought-activity-card.tsx
+++ src/components/wellness/thought-activity-card.tsx
@@ -1,53 +1,45 @@
 'use client'
 
-import { useState } from 'react'
+import { startTransition, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Textarea } from '@/components/ui/textarea'
-import { cn } from '@/lib/utils'
+import { PostLengthIndicator } from '../post-length-indicator'
+import { saveThought } from '@/actions/wellness'
 
 export function ThoughtActivityCard() {
-  const [thought, setThought] = useState<string>()
+  const [thought, setThought] = useState<string>('')
 
   const MAX_LENGTH = 280
 
   const handleThoughtChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setThought(e.target.value)
+    startTransition(() => setThought(e.target.value))
   }
 
-  function renderThoughtLength() {
-    if (thought) {
-      return thought.length > MAX_LENGTH
-        ? `${MAX_LENGTH - thought.length}`
-        : thought.length
-    }
-    return 0
+  function resetThought() {
+    startTransition(() => setThought(''))
   }
 
   return (
     <Card className='p-3'>
-      <form>
+      <form action={saveThought}>
         <Textarea
           placeholder="What's on your mind?"
           className='p-0 border-none shadow-none text-base focus-visible:ring-0'
           onChange={handleThoughtChange}
           value={thought}
           rows={6}
+          name='thought'
         />
 
         <div className='flex items-end justify-between pt-3'>
-          <span
-            className={cn('text-sm text-neutral-500', {
-              'text-red-600': thought && thought.length > MAX_LENGTH
-            })}
-          >
-            {renderThoughtLength()} / {MAX_LENGTH}
-          </span>
+          <PostLengthIndicator len={thought.length} max={MAX_LENGTH} />
 
           <Button
             size='sm'
-            disabled={!thought || thought?.length > MAX_LENGTH}
+            disabled={!thought || thought.length > MAX_LENGTH}
             type='submit'
+            onClick={resetThought}
           >
             Post
           </Button>
