7db81bc0dea5785a8b0a1a63b16d9de5fba98899
joelriveradev <joelriveradev@gmail.com>
2024-11-07 12:04:06 -0600

    feat: add visual feedback to show user has liked a post

--- src/components/like-button.tsx
+++ src/components/like-button.tsx
@@ -1,8 +1,16 @@
 'use client'
 
-import { useOptimistic, startTransition } from 'react'
+import {
+  useOptimistic,
+  startTransition,
+  useEffect,
+  useState,
+  useId
+} from 'react'
+
 import { ThumbsUp } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { createClient } from '@/lib/supabase/client'
 import { likePost } from '@/actions/wellness'
 import { cn } from '@/lib/utils'
 import { Like } from '@/types'
@@ -13,6 +21,10 @@ interface Props {
 }
 
 export function LikeButton({ postId, likes }: Props) {
+  const supabase = createClient()
+  const optimisticId = useId()
+
+  const [userId, setUserId] = useState<string>('')
   const [optimisiticLikes, setOptimisticLikes] = useOptimistic(
     likes,
     (prevLikes, value: Like) => {
@@ -25,15 +37,32 @@ export function LikeButton({ postId, likes }: Props) {
       ]
     }
   )
+
+  useEffect(() => {
+    async function getUser() {
+      const { data, error } = await supabase.auth.getUser()
+
+      if (error) {
+        console.error('Error fetching user:', error.message)
+      }
+
+      if (data?.user) {
+        startTransition(() => setUserId(data.user.id))
+      }
+    }
+
+    getUser()
+  }, [])
+
   const count = optimisiticLikes.length
 
   async function handleLike() {
     startTransition(() => {
       setOptimisticLikes({
+        id: optimisticId,
         post_id: postId,
         created_at: new Date().toISOString(),
-        user_id: '123',
-        id: Math.random().toString()
+        user_id: userId
       })
     })
 
@@ -51,12 +80,16 @@ export function LikeButton({ postId, likes }: Props) {
     <Button
       type='button'
       variant='ghost'
-      className={cn('w-full font-medium', {
-        'text-indigo-600': ''
-      })}
+      className='w-full font-medium hover:bg-gray-50 transition-all'
       onClick={handleLike}
     >
-      <ThumbsUp size={16} /> <LikesLabel likes={count} />
+      <ThumbsUp
+        size={16}
+        className={cn({
+          'text-blue-600': likes.some(({ user_id }) => user_id === userId)
+        })}
+      />{' '}
+      <LikesLabel likes={count} />
     </Button>
   )
 }
