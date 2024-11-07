239156aa8a04f4b5dcc34dd981dff636642dd178
joelriveradev <joelriveradev@gmail.com>
2024-11-07 11:17:16 -0600

    feat: add actions for activity page: - fetch posts - post comments - like posts - post thoughts

--- src/actions/wellness.ts
+++ src/actions/wellness.ts
@@ -3,6 +3,11 @@
 import { cookies } from 'next/headers'
 import { createClient } from '@/lib/supabase/server'
 import { revalidatePath } from 'next/cache'
+import { Comment, PostResponse } from '@/types'
+
+export async function revalidate(path: string) {
+  revalidatePath(path)
+}
 
 export async function fetchWaterActivity() {
   const supabase = createClient(cookies())
@@ -103,7 +108,7 @@ export async function saveStepsActivity(data: FormData) {
 
   try {
     await client.from('steps').insert({
-      count: steps,
+      count: Number(steps),
       date: new Date().toISOString()
     })
   } catch (error) {
@@ -111,7 +116,94 @@ export async function saveStepsActivity(data: FormData) {
       console.error(error.message)
     }
   } finally {
-    revalidatePath('/activity')
+    await revalidate('/activity')
+  }
+}
+
+export async function saveThought(data: FormData) {
+  const thought = data.get('thought') as string
+  const supabase = createClient(cookies())
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
+
+  await supabase.from('posts').insert({
+    content: thought,
+    type: 'thought',
+    name: user?.user_metadata.full_name,
+    avatar_url: user?.user_metadata.avatar_url
+  })
+  await revalidate('/activity')
+}
+
+export async function likePost(id: string) {
+  const supabase = createClient(cookies())
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
+
+  if (user) {
+    const { error } = await supabase.from('likes').insert({
+      post_id: id,
+      user_id: user.id
+    })
+
+    if (error) {
+      console.error(error.message)
+    }
+    await revalidate('/activity')
+  }
+}
+
+export async function postComment(state: Array<Comment>, data: FormData) {
+  const supabase = createClient(cookies())
+  const comment = data.get('comment') as string
+  const postId = data.get('post_id') as string
+
+  const {
+    error,
+    data: { user }
+  } = await supabase.auth.getUser()
+
+  if (user) {
+    const { data: comments } = await supabase
+      .from('comments')
+      .insert({
+        post_id: postId,
+        user_id: user.id,
+        content: comment,
+        name: user.user_metadata.full_name,
+        avatar_url: user.user_metadata.avatar_url
+      })
+      .select('*')
+
+    await revalidate('/activity')
+
+    if (error) {
+      console.error(error.message)
+    }
+
+    return state.concat(comments ?? [])
+  }
+  return state
+}
+
+export async function fetchPosts(): Promise<{ posts: Array<PostResponse> }> {
+  const supabase = createClient(cookies())
+
+  const { data: posts, error } = await supabase
+    .from('posts')
+    .select(`*, likes(user_id), comments(*)`)
+    .order('created_at', { ascending: false })
+
+  if (error) {
+    console.error(error.message)
+  }
+
+  return {
+    posts: posts as PostResponse[]
   }
 }
 
